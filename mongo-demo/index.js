1e22726345a26a8d82f93ea52bbd81c259a1267c
Homayoun <homayoun763@gmail.com>
2026-07-17 22:26:47 +0330

    Saving a Document

--- mongo-demo/index.js
+++ mongo-demo/index.js
@@ -1,21 +1,30 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-mongoose.connect('mongodb://localhost/playground')
-    .then(() => console.log("Connected to MongoDB..."))
-    .catch(err => console.error("Could not connect to MongoDB..", err))
+mongoose
+  .connect("mongodb://localhost:27017/playground")
+  .then(() => console.log("Connected to MongoDB..."))
+  .catch((err) => console.error("Could not connect to MongoDB..", err));
 
 const courseSchema = new mongoose.Schema({
-    name : String,
-    author : String,
-    tags : [String],
-    date : {type : Date, default : Date.now },
-    isPublished : Boolean
-})
+  name: String,
+  author: String,
+  tags: [String],
+  date: { type: Date, default: Date.now },
+  isPublished: Boolean,
+});
 
-const Course = mongoose.model('Course', courseSchema);
-const course = new Course({
-    name : "Node.js Course",
-    author : "Mosh",
-    tags: ['node', 'backend'],
-    isPUblished : true
-})
+const Course = mongoose.model("Course", courseSchema);
+
+async function createCourse() {
+  const course = new Course({
+    name: "Angular Course",
+    author: "Mosh",
+    tags: ["angular", "frontend"],
+    isPUblished: true,
+  });
+
+  const result = await course.save();
+  console.log(result);
+}
+
+createCourse();
