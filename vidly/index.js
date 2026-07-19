2ed819ab70f1ea1a4cab8d1aacf46a2cd5a28ce1
Homayoun <homayoun763@gmail.com>
2026-07-20 00:21:09 +0330

    vidly : Add Persistence to Genres API

--- vidly/index.js
+++ vidly/index.js
@@ -1,11 +1,16 @@
+const mongoose = require('mongoose')
 const express = require("express");
 const app = express();
 const genres = require('./routes/genres');
+
+mongoose.connect('mongodb://localhost/vidly')
+  .then(() => console.log('Connected to MongoDB...'))
+  .catch(err => console.log('Could not connect to MongoDB...'))
 
 app.use(express.json());
 app.use('/api/genres', genres)
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+app.listen(port, () => { 
   console.log(`Listening on port ${port}...`);
 });
