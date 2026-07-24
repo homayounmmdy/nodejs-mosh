0c794280863c3ef5dd5a54d5e9fc7e0ff3a1da4d
Homayoun <homayoun763@gmail.com>
2026-07-24 17:19:46 +0330

    Restructuring the Project

--- vidly/routes/customers.js
+++ vidly/routes/customers.js
@@ -1,26 +1,7 @@
-const Joi = require("joi");
+const mongoose = require("mongoose");
 const express = require("express");
 const router = express.Router();
-const mongoose = require("mongoose");
-
-const Customer = mongoose.model("Customer", {
-  name: {
-    type: String,
-    required: true,
-    minlength: 5,
-    maxlength: 50,
-  },
-  isGold: {
-    type: Boolean,
-    default: false,
-  },
-  phone: {
-    type: String,
-    required: true,
-    minlength: 5,
-    maxlength: 50,
-  },
-});
+const {Customer , validate} = require('../models/customer');
 
 router.get("/", async (req, res) => {
   const customers = await Customer.find().sort("name");
@@ -28,26 +9,16 @@ router.get("/", async (req, res) => {
 });
 
 router.post("/", async (req, res) => {
-  const { error } = validateCustomer(req.body);
+  const { error } = validate(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
   let customer = new Customer({
     name: req.body.name,
     phone: req.body.phone,
-    isGold: req.body.isGold
+    isGold: req.body.isGold,
   });
   customer = await genres.save();
   res.send(customer);
-}); 
-
-function validateCustomer(customer) {
-  const schema = {
-    name: Joi.string().min(5).max(50).required(),
-    phone: Joi.string().min(5).max(50).required(),
-    isGold: Joi.boolean(),
-  };
-
-  return Joi.validate(customer, schema);
-}
+});
 
 module.exports = router;
