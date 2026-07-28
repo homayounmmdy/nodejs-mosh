ff05d5368d8750e974dbccbdbf8870f41e32f8c5
Homayoun <homayoun763@gmail.com>
2026-07-28 20:55:47 +0330

    Transactions

--- vidly/routes/rentals.js
+++ vidly/routes/rentals.js
@@ -2,13 +2,14 @@ const { Rental, validate } = require("../models/rental");
 const { Movie } = require("../models/movie");
 const { Customer } = require("../models/customer");
 const mongoose = require("mongoose");
+const Fawn = require("fawn");
 const express = require("express");
 const router = express.Router();
 
+Fawn.init(mongoose);
 
 router.get("/", async (req, res) => {
-  const rentals = await Rental.find()
-    .sort("-dateOut");
+  const rentals = await Rental.find().sort("-dateOut");
   res.send(rentals);
 });
 
@@ -29,19 +30,29 @@ router.post("/", auth, async (req, res) => {
     customer: {
       _id: customer._id,
       name: customer.name,
-      phone: customer.phone
+      phone: customer.phone,
     },
     movie: {
       _id: movie._id,
       title: movie.title,
-      dailyRentalRate: movie.dailyRentalRate
-    }
+      dailyRentalRate: movie.dailyRentalRate,
+    },
   });
 
-  rental = await rental.save(); 
+  try {
+    new Fawn.Task()
+      .save("rentals", rental)
+      .update(
+        "movies",
+        { _id: movie._id },
+        {
+          $inc: { numberInStock: -1 },
+        },
+      )
+      .run();
 
-  movie.numberInStock--;
-  movie.save();
-
-  res.send(rental)
+    res.send(rental);
+  } catch (ex) {
+    res.status(500).send("Something failed.");
+  }
 });
