50e55f10c14a2d02cafea2c8052272ec4114f923
Homayoun <homayoun763@gmail.com>
2026-07-31 11:44:30 +0330

    Generating Authentication Tokens

--- vidly/routes/auth.js
+++ vidly/routes/auth.js
@@ -1,3 +1,4 @@
+const jwt = require('jsonwebtoken')
 const Joi = require("joi");
 const _ = require("lodash");
 const { User } = require("../models/user");
@@ -16,7 +17,8 @@ router.post("/", async (req, res) => {
   const validPassword = await bcrypt.compare(req.body.password, user.password)
   if (!validPassword) return res.status(400).send("Invalid email or password.");
 
-    res.send(true)
+  const token = jwt.sign({_id : user.id},'jwtPrivateKey');
+  res.send(token)
 });
 
 function validate(req) {
