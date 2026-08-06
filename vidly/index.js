3e1e17910a72c800551f463081b3954029c02793
Homayoun <homayoun763@gmail.com>
2026-08-06 18:17:59 +0330

    Express Error Middleware

--- vidly/index.js
+++ vidly/index.js
@@ -1,30 +1,34 @@
-const config = require('config')
-const Joi = require('joi');
-Joi.objectId = require('joi-objectid')(Joi)
-const mongoose = require('mongoose')
+const error = require("./middleware/error");
+const config = require("config");
+const Joi = require("joi");
+Joi.objectId = require("joi-objectid")(Joi);
+const mongoose = require("mongoose");
 const express = require("express");
 const app = express();
-const genres = require('./routes/genres');
-const customers = require('./routes/customers');
-const users = require('./routes/users');
-const auth = require('./routes/auth');
+const genres = require("./routes/genres");
+const customers = require("./routes/customers");
+const users = require("./routes/users");
+const auth = require("./routes/auth");
 
-if(!config.get('jwtPrivateKey')) {
-  console.error('FATAL ERROR: jwtPrivateKey is not defined.');
-  process.exit(1); 
+if (!config.get("jwtPrivateKey")) {
+  console.error("FATAL ERROR: jwtPrivateKey is not defined.");
+  process.exit(1);
 }
 
-mongoose.connect('mongodb://localhost/vidly')
-  .then(() => console.log('Connected to MongoDB...'))
-  .catch(err => console.log('Could not connect to MongoDB...'))
+mongoose
+  .connect("mongodb://localhost/vidly")
+  .then(() => console.log("Connected to MongoDB..."))
+  .catch((err) => console.log("Could not connect to MongoDB..."));
 
 app.use(express.json());
-app.use('/api/genres', genres)
-app.use('/api/customers', customers);
-app.use('/api/users', users);
-app.use('/api/auth', auth);
+app.use("/api/genres", genres);
+app.use("/api/customers", customers);
+app.use("/api/users", users);
+app.use("/api/auth", auth);
+
+app.use(error);
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => { 
+app.listen(port, () => {
   console.log(`Listening on port ${port}...`);
 });
