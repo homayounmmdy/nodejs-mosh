007fbbfccf33fe3589ef6bf5a7626148cc477f41
Homayoun <homayoun763@gmail.com>
2026-08-06 17:35:11 +0330

    Getting the Current User

--- vidly/routes/users.js
+++ vidly/routes/users.js
@@ -1,9 +1,15 @@
+const auth = require("../middleware/auth");
 const _ = require("lodash");
 const { User, validate } = require("../models/user");
 const bcrypt = require("bcrypt");
 const mongoose = require("mongoose");
 const express = require("express");
 const router = express.Router();
+
+router.get("/me", auth, async (req, res) => {
+  const user = await User.findById(req.user._id).select("-password");
+  res.send(user);
+});
 
 router.post("/", async (req, res) => {
   const { error } = validate(req.body);
@@ -17,7 +23,7 @@ router.post("/", async (req, res) => {
   user.password = await bcrypt.hash(user.password, salt);
 
   await user.save();
-  const token = user.generateAuthToken();;
+  const token = user.generateAuthToken();
   res
     .header("x-auth-token", token)
     .send(_.pick(user, ["_id", "name", "email"]));
