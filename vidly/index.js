7d37ea4e2d0382b7d98a5233c69bfd2b587b7524
Homayoun <homayoun763@gmail.com>
2026-08-07 15:19:31 +0330

    Extracting the Validation Logic

--- vidly/index.js
+++ vidly/index.js
@@ -1,5 +1,4 @@
-const Joi = require("joi");
-Joi.objectId = require("joi-objectid")(Joi);
+const winston = require("winston");
 const express = require("express");
 const app = express();
 
@@ -7,8 +6,9 @@ require("./startup/logging");
 require("./startup/routes")(app);
 require("./startup/db")();
 require("./startup/config")();
+require("./startup/validation")();
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log(`Listening on port ${port}...`);
+  winston.info(`Listening on port ${port}...`);
 });
