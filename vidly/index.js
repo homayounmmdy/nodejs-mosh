1eb271b9fb10f2679d4536fa333d9bd36afa0008
Homayoun <homayoun763@gmail.com>
2026-08-14 18:57:22 +0330

    Preparing the App for Production

--- vidly/index.js
+++ vidly/index.js
@@ -6,7 +6,8 @@ require("./startup/logging")();
 require("./startup/routes")(app);
 require("./startup/db")();
 require("./startup/config")();
-require("./startup/validation")();
+require("./startup/validation")(); 
+require("./startup/prod")(app);
 
 const port = process.env.PORT || 3000;
 const server = app.listen(port, () => {
