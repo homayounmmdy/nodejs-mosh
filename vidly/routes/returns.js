a31c23b608774fed70504b30be7ea7aa1491b7fe
Homayoun <homayoun763@gmail.com>
2026-08-14 16:53:00 +0330

    Testing the Response

--- vidly/routes/returns.js
+++ vidly/routes/returns.js
@@ -19,15 +19,18 @@ router.post("/", auth, async (req, res) => {
     return res.status(400).send("Return already processed.");
 
   rental.dateReturned = new Date();
-  const rentalDays = moment().diff(rental.dateOut , 'days');
-  rental.rentalFee = rentalDays * rental.movie.dailyRentalRate
+  const rentalDays = moment().diff(rental.dateOut, "days");
+  rental.rentalFee = rentalDays * rental.movie.dailyRentalRate;
   await rental.save();
 
-  await Movie.update({_id : rental.movie._id}, {
-    $inc: {numberInStock : 1}
-  });
+  await Movie.update(
+    { _id: rental.movie._id },
+    {
+      $inc: { numberInStock: 1 },
+    },
+  );
 
-  return res.status(200).send();
+  return res.status(200).send(rental);
 });
 
 module.exports = router;
