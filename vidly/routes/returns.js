133afac97b3de64c869ef5761f0c70c230cb7c72
Homayoun <homayoun763@gmail.com>
2026-08-14 15:42:07 +0330

    Looking Up an Object

--- vidly/routes/returns.js
+++ vidly/routes/returns.js
@@ -4,8 +4,13 @@ const router = express.Router();
 router.post("/", async (req, res) => {
   if (!req.body.customerId)
     return res.status(400).send("customerId not provided");
-  if (!req.body.movieId)
-    return res.status(400).send("movieId not provided");
+  if (!req.body.movieId) return res.status(400).send("movieId not provided");
+
+  const rental = await Rental.findOne({
+    "customer._id": req.body.customerId,
+    "movie._id": req.body.movieId, 
+  });
+  if (!result) return res.status(404).send("Rental not found.");
 
   res.status(401).send("Unauthorized");
 });
