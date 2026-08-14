0564c064d116db83dc667586f2646646060b725b
Homayoun <homayoun763@gmail.com>
2026-08-14 16:12:20 +0330

    Testing the Valid Request

--- vidly/routes/returns.js
+++ vidly/routes/returns.js
@@ -1,7 +1,9 @@
 const express = require("express");
+const { Rental } = require("../models/rental");
+const auth = require("../middleware/auth");
 const router = express.Router();
 
-router.post("/", async (req, res) => {
+router.post("/", auth, async (req, res) => {
   if (!req.body.customerId)
     return res.status(400).send("customerId not provided");
   if (!req.body.movieId) return res.status(400).send("movieId not provided");
@@ -14,8 +16,8 @@ router.post("/", async (req, res) => {
 
   if (rental.dateReturned)
     return res.status(400).send("Return already processed.");
-  
-  res.status(401).send("Unauthorized");
+
+  return res.status(200).send();
 });
 
 module.exports = router;
